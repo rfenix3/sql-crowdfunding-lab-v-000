c04b93e77a717378eec75f5c8171a168d6f99ba4
Richard Fenix <richard.fenix@gmail.com>
2018-07-20 20:18:00 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date NUMERIC, end_date NUMERIC);
+
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount NUMBERIC, 
+  user_id INTEGER, project_id INTEGER);
+
